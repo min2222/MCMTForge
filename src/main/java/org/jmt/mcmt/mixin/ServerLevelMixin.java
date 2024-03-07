543ace07e720f35a3ebea6896d97f24f3626bc9c
min <goyoumin2@gmail.com>
2024-03-07 11:27:15 +0900

    fix

--- src/main/java/org/jmt/mcmt/mixin/ServerLevelMixin.java
+++ src/main/java/org/jmt/mcmt/mixin/ServerLevelMixin.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.Executor;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 import org.jmt.mcmt.asmdest.ASMHookTerminator;
@@ -71,8 +72,7 @@ public abstract class ServerLevelMixin implements WorldGenLevel {
     	ASMHookTerminator.callEntityTick(consumer, entity, thisWorld);
     }
     
-    //handled in core mod
-    /*@Redirect(method = "blockEvent", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectLinkedOpenHashSet;add(Ljava/lang/Object;)Z"))
+    @Redirect(method = "blockEvent", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectLinkedOpenHashSet;add(Ljava/lang/Object;)Z"))
     private boolean overwriteQueueAdd(ObjectLinkedOpenHashSet<BlockEventData> objectLinkedOpenHashSet, Object object) {
         return syncedBlockEventCLinkedQueue.add((BlockEventData) object);
     }
@@ -80,7 +80,7 @@ public abstract class ServerLevelMixin implements WorldGenLevel {
     @Redirect(method = "clearBlockEvents", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectLinkedOpenHashSet;removeIf(Ljava/util/function/Predicate;)Z"))
     private boolean overwriteQueueRemoveIf(ObjectLinkedOpenHashSet<BlockEventData> objectLinkedOpenHashSet, Predicate<BlockEventData> filter) {
         return syncedBlockEventCLinkedQueue.removeIf(filter);
-    }*/
+    }
 
     @Redirect(method = "runBlockEvents", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectLinkedOpenHashSet;isEmpty()Z"))
     private boolean overwriteEmptyCheck(ObjectLinkedOpenHashSet<BlockEventData> objectLinkedOpenHashSet) {
